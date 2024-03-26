47dd7e96d92eae243386b569c5ac9a0f9af2c8de
dev-mdshahid <mdshahidulridoy@gmail.com>
2024-03-26 22:10:10 +0600

    initial setup

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -6,9 +6,17 @@ import { Text, View } from '@/components/Themed';
 export default function TabOneScreen() {
   return (
     <View style={styles.container}>
-      <Text style={styles.title}>Tab One</Text>
-      <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
-      <EditScreenInfo path="app/(tabs)/index.tsx" />
+      <Text className="text-orange-600 text-2xl font-bold">🍔 Hi FoodyBhai! 🍔</Text>
+      <View
+        style={styles.separator}
+        lightColor="#eee"
+        darkColor="rgba(255,255,255,0.1)"
+      />
+      <Text className="bg-orange-600 text-center p-5 text-base text-white rounded-xl m-2">
+        Feeling super hungry but don't want to step out of your home? Don't
+        worry! FoodyBhai is here to satisfy your apetite 👅👅
+      </Text>
+      {/* <EditScreenInfo path="app/(tabs)/index.tsx" /> */}
     </View>
   );
 }
@@ -20,7 +28,8 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
   },
   title: {
-    fontSize: 20,
+    color: '#fe724c',
+    fontSize: 22,
     fontWeight: 'bold',
   },
   separator: {
